040834e4f87b9590ffe0091653430e5d1413cd87
Rajab <rkarume@gmail.com>
2022-11-07 22:47:18 +0300

    completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+    def favorite_product
+        reviews.order(:star_rating).last.product
+    end
+    def remove_reviews(products)
+        reveiws.where(product_id: product.id).destroy_all
+    end
 end
